90dbf6516d556cfc49a2b714c78f38dfc94b8cc8
Andrey Terekhov <satyr663@gmail.com>
2023-01-19 10:33:32 +0600

    feat: add initial layout

---
+++ src/components/HomePage/HomePage.tsx
@@ -0,0 +1,9 @@
+import { Container } from '@mui/material';
+import Link from 'next/link';
+import CurrencySelect from '../CurrencySelect/CurrencySelect';
+
+// interface HomePageProps extends PageProps {}
+
+const HomePage = () => <CurrencySelect />;
+
+export default HomePage;
---
+++ src/components/MainLayout/MainLayout.tsx
@@ -0,0 +1,23 @@
+import React, { HTMLAttributes, PropsWithChildren } from 'react';
+// import Footer from '../Footer/Footer';
+// import Header from '../Header/Header';
+import Container from '@mui/material/Container';
+import styles from './MainLayout.module.scss';
+
+interface MainLayoutProps extends HTMLAttributes<HTMLDivElement> {}
+
+const MainLayout: React.FC<PropsWithChildren<MainLayoutProps>> = (props) => {
+  return (
+    <div className={props.className}>
+      <div className={styles.wrapper}>
+        {/* <Header /> */}
+        <main className={styles.main}>
+          <Container maxWidth="lg">{props.children}</Container>
+        </main>
+        {/* <Footer /> */}
+      </div>
+    </div>
+  );
+};
+
+export default MainLayout;
